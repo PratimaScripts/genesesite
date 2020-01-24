b339717adc6e4c092e9e05d6c885ab51632dc620
Laxmi Gurung <samrita.grg20@gmail.com>
2020-01-24 11:26:00 +0545

    changes css to scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import Navigation from "./Navigation";
 import Carousel from "./CarouselSlider";
 import Homepage from "./Homepage/Homepage";
