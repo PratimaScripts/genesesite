2c9fb8e4cc83127b11c60089129c4d9e1d050c7e
Thomas Field <thomasrgfield@gmail.com>
2020-02-27 12:29:07 +0000

    Digital Transformation text update inline with new wireframe

--- src/Services/DigitalTransformation.js
+++ src/Services/DigitalTransformation.js
@@ -6,21 +6,15 @@ import DigitalTransformationImage from "./digital-transformation.png";
 
 export default function DigitalTransformation() {
   useEffect(() => {
-    window.scrollTo(0, 0)
- }, [])
- 
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <div className="section bg-primary text-white">
         <Container>
           <h1 className="">Digital Transformation</h1>
-          <p>
-            As digital transformation experts, our team of certified Architects,
-            Developers and DevOps professionals merge technology with strategy.
-            We'll create a personalised cloud adoption strategy that offers the
-            best solution for your business, without disrupting your ongoing
-            operations.
-          </p>
+          <h1 className="">Digital Transformation</h1>
         </Container>
       </div>
       <div className="section p-0 bg-white" id="product">
@@ -37,26 +31,45 @@ export default function DigitalTransformation() {
             <Col lg={6} className="p-0">
               <div className="section-content right text-left">
                 <p>
-                  Let us transition your existing environments to the cloud so
-                  that you can do away with outdated processes and legacy
+                  Cloud services form the foundation of Digital Transformation
+                  and facilitate rapid business change, in a world that is
+                  always changing, your business has to be able to adapt in
+                  order to be competitive.
+                </p>
+                <p>
+                  As Digital transformation experts, our team merge technology
+                  with strategy. We create a personalised digital adoption plan,
+                  that provides the best cloud solutions, meeting your business
+                  needs without disrupting ongoing operations.
+                </p>
+                <p>
+                  Let us transition your existing IT environments to the cloud
+                  so that you can do away with outdated processes and legacy
                   technology. We specialise in building highly available,
-                  infrastructure and storage. We offer private and hybrid cloud
-                  solutions, that accommodate diverse workloads with low latency
-                  and robust disaster recovery.
+                  infrastructure and storage. Our private and hybrid cloud
+                  solutions accommodate diverse workloads with low latency and
+                  robust disaster recovery.
                 </p>
                 <p>
-                  We'll integrate digital technology into multiple areas of your
-                  organisation to optimise how you operate. Leverage data
-                  analytics and automation to handle time-consuming tasks, add
-                  value and improve the experience for your customers, suppliers
-                  and employees.
+                  We'll integrate digital technologies into multiple areas of
+                  your organisation to optimise how you operate. Automate
+                  time-consuming tasks using Artificial Intelligence, adding
+                  value and improving the experience for your customers,
+                  suppliers and employees.
                 </p>
                 <p>
-                  Genese Solution will help you become more agile, deliver
-                  features faster and at scale, through integrated continuous
-                  delivery pipelines, API's and serverless architecture. Improve
-                  collaboration with enterprise technology, make workflows and
-                  file transfers across your cloud environment.
+                  Collect, analyse and utilise business Big Data through IoT.
+                  Leverage data-driven insights to enable new service offerings,
+                  create new earning potential, reduce expenditure and optimise
+                  processes.
+                </p>
+                <p>
+                  Our Digital Transformation will help your organisation to
+                  become more agile, deliver features faster and at scale,
+                  through integrated continuous delivery pipelines, API's and
+                  serverless architecture. Improve collaboration with enterprise
+                  technology, make workflows and file transfers across your
+                  cloud environment.
                 </p>
               </div>
             </Col>
@@ -66,15 +79,9 @@ export default function DigitalTransformation() {
       <div className="section bg-light" id="service">
         <Container>
           <p>
-            Run your business with increased security and compliance. Our AWS
-            certified Security Specialists understand how important control,
-            privacy and data protection are to your business. We'll architect
-            your infrastructure in line with best practices so that you can rest
-            assured.
-          </p>
-          <p>
             <a href="#/contact">
-              Let's discuss your digital transformation needs
+              Take the first step towards your Digital Transformation and
+              arrange a free consutlation.
             </a>
           </p>
         </Container>
